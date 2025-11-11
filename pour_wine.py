3ec02d7bf25ed7eb41f7565ff057327f7426f137
Kam Ho <71874669+Leung-Kam-Ho@users.noreply.github.com>
2025-11-11 19:08:25 +0800

    Refactor pouring sequence: update positions and simplify behavior tree structure

--- pour_wine.py
+++ pour_wine.py
@@ -2,9 +2,11 @@ from HansRobot_Elfin_robot_arm.robot_arm import *
 import time
 import atexit
 import py_trees
+from py_trees.decorators import Repeat
 from py_trees.composites import Sequence, Selector
 from py_trees.behaviours import Success
 from py_trees.common import Status
+from math import inf
 
 
 class MoveToPosition(py_trees.behaviour.Behaviour):
@@ -92,35 +94,45 @@ def main():
     
     # Define positions
     positions = {
+        'zero': JPosition(0, 0, 0, 0, 0, 0),
         'home': JPosition(-65.127, -44.867, 130.668, -34.806, -86.075, 3.022),
-        'infront_cup': JPosition(-94, -56, 89, -68, -75, 29),
-        'around_cup': JPosition(-83.147, -67.820, 69.243, -60.232, -66.391, 36.582),
+        'infront_beer': JPosition(-94, -56, 89, -68, -75, 29),
+        'around_beer': JPosition(-83.147, -67.820, 69.243, -60.232, -66.391, 36.582),
         'ready_pour': JPosition(-73.174, -46.835, 91.888, -54.300, -59.557, 33.708),
-        'pour': JPosition(-73.187, -46.839, 91.874, -54.319, -59.557, -62.221)
+        'pour': JPosition(-73.187, -46.839, 91.874, -54.319, -59.557, -62.221),
+        'around_cup' : JPosition(-54.194,-54.647,103.428,-25.451,-69.755,9.709),
+        'ready_cup_pour': JPosition(-77.645,-47.430,84.537,-58.462,-59.464,39.802)
     }
-    
+    normal_speed = 0.5
     # Build behavior tree
     root = Sequence("PouringSequence", memory=True)
     
     root.add_children([
-        MoveGripper(ra, 140, "Open Gripper"),
-        SetSpeed(ra, 1, "Set Normal Speed"),
-        MoveToPosition(ra, positions['home'], "Move to Home"),
-        MoveToPosition(ra, positions['infront_cup'], "Move In Front of Cup"),
-        MoveToPosition(ra, positions['around_cup'], "Move Around Cup"),
-        MoveGripper(ra, 0, "Close Gripper"),
-        MoveToPosition(ra, positions['ready_pour'], "Move to Ready Pour"),
-        SetSpeed(ra, 0.3, "Set Slow Speed"),
-        MoveToPosition(ra, positions['pour'], "Pour"),
-        Wait(2, "Wait for Pouring"),
-        SetSpeed(ra, 1, "Set Normal Speed"),
-        MoveToPosition(ra, positions['ready_pour'], "Move to Ready Position"),
-        MoveToPosition(ra, positions['around_cup'], "Move Around Cup"),
-        MoveGripper(ra, 140, "Open Gripper"),
-        MoveToPosition(ra, positions['infront_cup'], "Move In Front of Cup"),
-        MoveToPosition(ra, positions['home'], "Move to Home")
+        MoveToPosition(ra, positions['zero'], "Move to Zero Position"),
+        # MoveGripper(ra, 0, "Open Gripper"),
+        # MoveGripper(ra, 140, "Open Gripper"),
+        # SetSpeed(ra, normal_speed, "Set Normal Speed"),
+        # MoveToPosition(ra, positions['home'], "Move to Home"),
+        # MoveToPosition(ra, positions['infront_beer'], "Move In Front of Cup"),
+        # MoveToPosition(ra, positions['around_beer'], "Move Around Cup"),
+        # MoveGripper(ra, 0, "Close Gripper"),
+        # MoveToPosition(ra, positions['ready_pour'], "Move to Ready Pour"),
+        # SetSpeed(ra, 0.3, "Set Slow Speed"),
+        # MoveToPosition(ra, positions['pour'], "Pour"),
+        # Wait(2, "Wait for Pouring"),
+        # SetSpeed(ra, normal_speed, "Set Normal Speed"),
+        # MoveToPosition(ra, positions['ready_pour'], "Move to Ready Position"),
+        # MoveToPosition(ra, positions['around_beer'], "Move Around Cup"),
+        # MoveGripper(ra, 140, "Open Gripper"),
+        # MoveToPosition(ra, positions['infront_beer'], "Move In Front of Cup"),
+        # MoveToPosition(ra, positions['home'], "Move to Home"),
+        # MoveToPosition(ra, positions['around_cup'], "Move Around Cup"),
+        # MoveGripper(ra, 0, "Close Gripper"),
+        # MoveToPosition(ra, positions['ready_cup_pour'], "Ready for Next Pour")
+        
     ])
     
+    # root = Repeat( name="Repeat Pouring Sequence", child=root, num_success=inf)
     # Execute behavior tree
     root.setup_with_descendants()
     
